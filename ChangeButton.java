33dd8ce3b915ccbbb119149215df4027a0ce4c48
Rumburaq2 <93189782+Rumburaq2@users.noreply.github.com>
2023-04-14 21:30:45 +0200

    Add files via upload

---
+++ ChangeButton.java
@@ -0,0 +1,30 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class ChangeButton {
+
+    public static void main(String[] args) {
+        new ChangeButton();
+    }
+
+    public ChangeButton() {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+                } catch (ClassNotFoundException | InstantiationException | IllegalAccessException |
+                         UnsupportedLookAndFeelException ex) {
+                }
+
+                JFrame frame = new JFrame("Testing");
+                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+                frame.setLayout(new BorderLayout());
+                frame.add(new TestPane());
+                frame.pack();
+                frame.setLocationRelativeTo(null);
+                frame.setVisible(true);
+            }
+        });
+    }
+}
